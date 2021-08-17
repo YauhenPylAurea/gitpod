ef9400d4fac9a7bb53249ea8850ed8cfe9de913d
Laurie T. Malau <laurie@gitpod.io>
2021-08-17 10:39:44 +0000

    Try DB query

--- components/server/src/user/newsletter-subscription-controller.ts
+++ components/server/src/user/newsletter-subscription-controller.ts
@@ -4,17 +4,28 @@
  * See License-AGPL.txt in the project root for license information.
  */
 
-import { injectable } from "inversify";
 import * as express from 'express';
+import { inject, injectable } from "inversify";
+import { DBIdentity, UserDB } from "../../../gitpod-db/lib";
 
 @injectable()
 export class NewsletterSubscriptionController {
+    @inject(UserDB) protected readonly userDb: UserDB;
+    @inject(DBIdentity) protected readonly identityDb: DBIdentity;
+
     get apiRouter(): express.Router {
         const router = express.Router();
 
-        router.get("/unsubscribe", (req: express.Request, res: express.Response) => {
+        router.get("/unsubscribe", async (req: express.Request, res: express.Response) => {
 
-            res.send("Newsletter subscription controller");
+            console.log(req.query)
+            const email: string = req.query.email;
+            const newsletterType: string = req.query.type;
+
+            const userResults = await this.userDb.findUsersByEmail(email);
+
+            res.send(`${newsletterType} subscription for ${email} found ${userResults.length} which is ${userResults}`);
+
             // if emailAddress exists in User database:
             //      1. update the database
             //      2. send track event
