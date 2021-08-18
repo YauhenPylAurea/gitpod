30fb613f0d5ad1fde96c3c87a6f0d1f4329e00c8
Laurie T. Malau <laurie@gitpod.io>
2021-08-18 08:29:53 +0000

    Push to trigger

--- components/server/src/user/newsletter-subscription-controller.ts
+++ components/server/src/user/newsletter-subscription-controller.ts
@@ -25,14 +25,6 @@ export class NewsletterSubscriptionController {
             const userResults = await this.userDb.findUsersByEmail(email);
 
             res.send(`${newsletterType} subscription for ${email} found ${userResults.length} which is ${userResults}`);
-
-            // if emailAddress exists in User database:
-            //      1. update the database
-            //      2. send track event
-            //      3. redirect to page
-            // if does not exist:
-            //        1. send track event
-            //      2. redirect to page
         })
 
         return router;
